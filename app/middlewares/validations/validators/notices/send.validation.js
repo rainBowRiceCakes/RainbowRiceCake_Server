7ba7ece4903c18667ccbe4d9f7bf9ee5521b7090
uiwook <dmldnr1230@yu.ac.kr>
2025-12-23 15:48:23 +0900

    251223 파일명 실수로 수정

--- app/middlewares/validations/validators/notices/send.validation.js
+++
@@ -1,9 +0,0 @@
-/**
- * @file app/middlewares/validations/validators/create.validation.js
- * @description 공지발송 검사 핸들러
- * 251223 v1.0.0 wook init
- */
-
-import { adminId, content, status, targetRole, title } from '../../fields/notice.field.js';
-
-export default [ adminId, content, status, targetRole, title ];
