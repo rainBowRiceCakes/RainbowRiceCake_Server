6d5dfb6d00d5b02c2a44ee7fede29a9dbf48e0d7
uiwook <dmldnr1230@yu.ac.kr>
2026-02-12 14:09:39 +0900

    260212 project end로 인한 scheduler stop

--- app/schedulers/autoPay.scheduler.js
+++ app/schedulers/autoPay.scheduler.js
@@ -13,19 +13,20 @@ export const initAutoPayScheduler = () => {
   const rule = '0 3 10 * *';
   // const rule = '40 22 * * *';
 
-  scheduleJob(rule, async function () {
-    console.log('[Scheduler] 정기 자동 결제 스케줄 작업 시작...');
+  // TODO : 프로젝트 종료로 인한 스케쥴러 중지
+  // scheduleJob(rule, async function () {
+  //   console.log('[Scheduler] 정기 자동 결제 스케줄 작업 시작...');
 
-    try {
-      // 서비스에 정의된 배치 로직만 깔끔하게 호출
-      const result = await settlementsService.processBatchAutoPay();
+  //   try {
+  //     // 서비스에 정의된 배치 로직만 깔끔하게 호출
+  //     const result = await settlementsService.processBatchAutoPay();
 
-      console.log(`[Scheduler] 작업 종료 - 성공: ${result.successCount}건`);
-    } catch (error) {
-      // 서비스 레이어에서 던져진 치명적 에러 처리 (로그 기록 등)
-      console.error('[Scheduler] 배치 작업 실행 중 중단 에러:', error.message);
-    }
-  });
+  //     console.log(`[Scheduler] 작업 종료 - 성공: ${result.successCount}건`);
+  //   } catch (error) {
+  //     // 서비스 레이어에서 던져진 치명적 에러 처리 (로그 기록 등)
+  //     console.error('[Scheduler] 배치 작업 실행 중 중단 에러:', error.message);
+  //   }
+  // });
 
   console.log(`[Scheduler] 자동 결제 작업이 등록되었습니다. (실행 주기: ${rule})`);
 };
--- app/schedulers/invoice.schedule.js
+++ app/schedulers/invoice.schedule.js
@@ -9,18 +9,19 @@ import invoicesService from '../services/invoices.service.js';
 export const initInvoiceScheduler = () => {
   // Cron 표현식: 0 0 10 * * (매월 10일 00시 00분 실행)
   // 테스트용: */10 * * * * * (10초마다 실행)
-  const rule = '0 0 10 * *'; 
-  // const rule = '*/10 * * * * *'; 
+  const rule = '0 0 10 * *';
+  // const rule = '*/10 * * * * *';
 
-  schedule.scheduleJob(rule, async () => {
-    console.log('============== [Auto Invoice Scheduler Start] ==============');
-    try {
-      const result = await invoicesService.sendMonthlyInvoicesToAll();
-      console.log(`[Auto Invoice Scheduler End] Result:`, result);
-    } catch (error) {
-      console.error('[Auto Invoice Scheduler Error]', error);
-    }
-  });
+  // TODO : 프로젝트 종료로 인한 스케쥴러 중지
+  // schedule.scheduleJob(rule, async () => {
+  //   console.log('============== [Auto Invoice Scheduler Start] ==============');
+  //   try {
+  //     const result = await invoicesService.sendMonthlyInvoicesToAll();
+  //     console.log(`[Auto Invoice Scheduler End] Result:`, result);
+  //   } catch (error) {
+  //     console.error('[Auto Invoice Scheduler Error]', error);
+  //   }
+  // });
 
   console.log(`[Scheduler] Invoice job scheduled for: ${rule}`);
 };
